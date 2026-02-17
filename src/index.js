014afbd0a9508e418c4ee6401c3953c550b3b215
Ibrahimhaneefmvp <143109783+Ibrahimhaneefmvp@users.noreply.github.com>
2026-02-18 00:32:09 +0530

    Initialize React application in index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import ihanif from "./irshad-hanif";
+import "./index.css";
+
+const root = createRoot(document.getElementById("root"));
+root.render(<Doxy />);
