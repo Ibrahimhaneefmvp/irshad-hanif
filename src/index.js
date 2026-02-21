0f653602eeda76573aa27c6cb8b5c55d86ad3399
Ibrahimhaneefmvp <143109783+Ibrahimhaneefmvp@users.noreply.github.com>
2026-02-21 15:28:04 +0530

    Fix import statement for IrshadHanif component

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import irshad-hanif from "./irshad-hanif";  //
+import IrshadHanif.jsx from "./IrshadHanif";  //
 import "./index.css";
 
 const root = createRoot(document.getElementById("root"));
